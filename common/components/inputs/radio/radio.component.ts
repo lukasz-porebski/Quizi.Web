8bf121b7c817850fc8ccdba31d1ad4effa4ead81
Łukasz Porębski <lukaporebski@gmail.com>
2025-09-12 18:03:16 +0200

    Added template pass to radio component

--- common/components/inputs/radio/radio.component.ts
+++ common/components/inputs/radio/radio.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, input } from '@angular/core';
+import { Component, effect, Input, input, TemplateRef } from '@angular/core';
 import { MatRadioButton, MatRadioGroup } from '@angular/material/radio';
 import { MatLabel } from '@angular/material/form-field';
 import { TextConfigTranslatePipe } from '../../../pipes/text-config-translation.pipe';
@@ -8,6 +8,7 @@ import { ConditionalTranslatePipe } from '../../../pipes/conditional-translation
 import { isDefined } from '../../../utils/utils';
 import { ITextConfig, TextConfig } from '../../../models/text.config';
 import { RadioLabelPosition } from './enums/label-position.enum';
+import { NgIf, NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-radio',
@@ -18,25 +19,29 @@ import { RadioLabelPosition } from './enums/label-position.enum';
     ReactiveFormsModule,
     MatRadioButton,
     ConditionalTranslatePipe,
+    NgTemplateOutlet,
+    NgIf,
   ],
   templateUrl: './radio.component.html',
   styleUrl: './radio.component.scss',
 })
-export class RadioComponent<T> {
-  public formControl = input.required<FormControl<T>>();
-  public options = input.required<T[]>();
+export class RadioComponent<TData, TValue = TData> {
+  @Input() optionTpl?: TemplateRef<any>;
+
+  public formControl = input.required<FormControl<TValue>>();
+  public options = input.required<TData[]>();
   public label = input<ITextConfig>();
   public readonly = input<boolean>(false);
   public optionLabelPosition = input<RadioLabelPosition>(RadioLabelPosition.After);
   public translateOptionLabel = input<boolean>(false);
   public color = input<InputColor>(InputColor.Default);
-  public optionText = input<(option: T) => string>();
+  public optionText = input<(option: TData) => string>();
 
   public getRadioButtonColorClass(color: InputColor): string {
     return 'app-radio-button-' + color;
   }
 
-  public getOptionText(option: T): string {
+  public getOptionText(option: TData): string {
     return isDefined(this.optionText()) ? this.optionText()!(option) : (option?.toString() ?? '');
   }
 
