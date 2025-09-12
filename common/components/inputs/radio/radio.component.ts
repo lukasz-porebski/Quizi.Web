9ea3f5e31254e2dbe6ad909aada6dbb4dd04fcb6
Łukasz Porębski <lukaporebski@gmail.com>
2025-09-12 17:41:31 +0200

    Refactored radio component

--- common/components/inputs/radio/radio.component.ts
+++ common/components/inputs/radio/radio.component.ts
@@ -1,12 +1,13 @@
-import { Component, input } from '@angular/core';
+import { Component, effect, input } from '@angular/core';
 import { MatRadioButton, MatRadioGroup } from '@angular/material/radio';
-import { RadioConfig } from './models/radio.config';
 import { MatLabel } from '@angular/material/form-field';
 import { TextConfigTranslatePipe } from '../../../pipes/text-config-translation.pipe';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { InputColor } from '../shared/enums/input-color.enum';
 import { ConditionalTranslatePipe } from '../../../pipes/conditional-translation.pipe';
 import { isDefined } from '../../../utils/utils';
+import { ITextConfig, TextConfig } from '../../../models/text.config';
+import { RadioLabelPosition } from './enums/label-position.enum';
 
 @Component({
   selector: 'app-radio',
@@ -22,13 +23,28 @@ import { isDefined } from '../../../utils/utils';
   styleUrl: './radio.component.scss',
 })
 export class RadioComponent<T> {
-  public config = input.required<RadioConfig<T>>();
+  public formControl = input.required<FormControl<T>>();
+  public options = input.required<T[]>();
+  public label = input<ITextConfig>();
+  public readonly = input<boolean>(false);
+  public optionLabelPosition = input<RadioLabelPosition>(RadioLabelPosition.After);
+  public translateOptionLabel = input<boolean>(false);
+  public color = input<InputColor>(InputColor.Default);
+  public optionText = input<(option: T) => string>();
 
   public getRadioButtonColorClass(color: InputColor): string {
     return 'app-radio-button-' + color;
   }
 
-  public getOptionText(option: T): string | T {
-    return isDefined(this.config().optionText) ? this.config().optionText!(option) : option;
+  public getOptionText(option: T): string {
+    return isDefined(this.optionText()) ? this.optionText()!(option) : (option?.toString() ?? '');
+  }
+
+  public innerLabel?: TextConfig;
+
+  public constructor() {
+    effect(() => {
+      this.innerLabel = isDefined(this.label()) ? new TextConfig(this.label()!) : undefined;
+    });
   }
 }
