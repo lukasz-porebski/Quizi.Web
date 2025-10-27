6d7e5dcd49353e9ecf84f7b57ebc37c0a20f31af
Łukasz Porębski <lukaporebski@gmail.com>
2025-10-27 20:07:05 +0100

    Fixes

--- start.sh
+++ start.sh
@@ -5,7 +5,11 @@ echo "Generating Nginx config with:"
 echo "  API_URL=$API_URL"
 echo "  PORT=$PORT"
 
-envsubst '$API_URL $PORT' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+# Extract hostname from API_URL
+export API_HOST=$(echo "$API_URL" | sed -e 's|^https\?://||' -e 's|/.*$||')
+echo "  API_HOST=$API_HOST"
+
+envsubst '$API_URL $PORT $API_HOST' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 
 echo "Final Nginx config:"
 cat /etc/nginx/conf.d/default.conf
