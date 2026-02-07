5cba2e9d8f8cadca0571b690bd064949c8f18e95
Łukasz Porębski <lukaporebski@gmail.com>
2026-02-07 19:44:06 +0100

    Updated packages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,30 +2,24 @@ import { ApplicationConfig, ErrorHandler, provideZoneChangeDetection } from '@an
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideTranslateService, TranslateLoader } from '@ngx-translate/core';
-import { HTTP_INTERCEPTORS, HttpClient, provideHttpClient, withInterceptorsFromDi, } from '@angular/common/http';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { provideTranslateService } from '@ngx-translate/core';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AuthenticationInterceptor } from '@common/identity/services/authentication.interceptor';
 import { AppErrorHandler } from '@common/handlers/error.handler';
-
-const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: HttpClient) =>
-  new TranslateHttpLoader(http, './i18n/', '.json');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideTranslateService({
-      defaultLanguage: 'pl',
-    }),
     provideHttpClient(withInterceptorsFromDi()),
     provideTranslateService({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: httpLoaderFactory,
-        deps: [ HttpClient ],
-      },
+      fallbackLang: 'pl',
+    }),
+    provideTranslateHttpLoader({
+      prefix: './i18n/',
+      suffix: '.json'
     }),
     {provide: ErrorHandler, useClass: AppErrorHandler},
     {provide: HTTP_INTERCEPTORS, useClass: AuthenticationInterceptor, multi: true},
